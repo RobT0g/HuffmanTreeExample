ea0fa754dba8f0c6e1f69d1359eb6312fdc40e77
Robson Luan <64618248+RobT0g@users.noreply.github.com>
2024-12-17 16:17:36 -0300

    Update huffman.py

--- huffman.py
+++ huffman.py
@@ -2,6 +2,7 @@ import typing
 import matplotlib.pyplot as plt
 import networkx as nx
 import numpy as np
+import json
 
 class Node:
     def __init__(self, value:str|None, weight:int):
@@ -58,43 +59,17 @@ class BalancedList:
         return self.__list.copy()
 
 
-    def __binary_search_helper(lst, value, low, high) -> typing.Tuple[bool, int, int]:
-        #print(lst, value, low, high)
-        if low > high:
-            insertPos = BalancedList.__findWhereToInsert(lst, value, low, high)
-            return False, insertPos, insertPos
-
-        mid = (low + high) // 2
-
-        if lst[mid] == value:
-            boundaries = [mid, mid]
-
-            for step in range(mid, -1, -1):
-                if lst[step] == value: continue
-                boundaries[0] = step+1
-                break
-
-            for step in range(mid, len(lst)):
-                if lst[step] == value: continue
-                boundaries[1] = step-1
-                break
-
-            return True, boundaries[0], boundaries[1]
-        
-        if low == high:
-            insertPos = BalancedList.__findWhereToInsert(lst, value, low, high)
-            return False, insertPos, insertPos
-
-        if value < lst[mid]:
-            return BalancedList.__binary_search_helper(lst, value, low, mid - 1)
-        
-        return BalancedList.__binary_search_helper(lst, value, mid + 1, high)
-
 class HuffmanTree:
     def __init__(self, text:str, balance:bool=True):
         self.originalText:str = text
         self.root = None
-        if balance: self.balanceTree()
+        self.basicBinaryText = ''
+        self.huffmanBinaryText = ''
+        self.huffmanDictionary = {}
+        self.weightDictionary = {}
+        if balance: 
+            self.balanceTree()
+            self.mapDictionary()
 
     def balanceTree(self):
         count:dict = HuffmanTree.getCharacterCount(self.originalText)
@@ -112,14 +87,14 @@ class HuffmanTree:
             orderedAmounts.insertOrdered(count[i])
             characterPos.append(None)
 
-        print(orderedAmounts.getList())
+        #print(orderedAmounts.getList())
 
         for i in count:
             check = orderedAmounts.checkContain(count[i])
             helperAdd(Node(i, count[i]), check[1])
         
         while len(characterPos) > 1:
-            print([(i.value, i.weight) for i in characterPos])
+            #print([(i.value, i.weight) for i in characterPos])
             newNode = Node(None, characterPos[0].weight + characterPos[1].weight)
             newNode.assignLeft(characterPos[0])
             newNode.assignRight(characterPos[1])
@@ -130,9 +105,36 @@ class HuffmanTree:
             orderedAmounts.dropVal(0)
 
             newNodePos = orderedAmounts.insertOrdered(newNode.weight)
-            characterPos.insert(newNodePos[0], newNode)
+            characterPos.insert(newNodePos[1], newNode)
         
         self.root = characterPos[0]
+
+    def calculateConversionDictionary(self):
+        self.mapDictionary()
+        with open('HuffmanDictionary.json', 'w') as file:
+            json.dump(self.huffmanDictionary, file, indent=4)
+
+    def mapDictionary(self, currentStep:Node|None=None, binaryString:str=''):
+        if currentStep is None:
+            currentStep = self.root
+        if currentStep.getLeftBranch() is None and currentStep.getRightBranch() is None:
+            self.huffmanDictionary[currentStep.value] = binaryString
+            self.weightDictionary[currentStep.value] = currentStep.weight
+            return
+        self.mapDictionary(currentStep.getLeftBranch(), binaryString+'0')
+        self.mapDictionary(currentStep.getRightBranch(), binaryString+'1')
+
+    def convertIntoBinary(self) -> str:
+        converted = ''
+        for i in self.originalText:
+            converted += HuffmanTree.convertFromCharToBinary(i)
+        return converted
+
+    def convertIntoHuffmanBinary(self) -> str:
+        converted = ''
+        for i in self.originalText:
+            converted += self.huffmanDictionary[i]
+        return converted
 
     def plotTreeBasic(self, currentStep:Node|None = None, level:int=0):
         if currentStep is None:
@@ -151,7 +153,7 @@ class HuffmanTree:
         graph.add_node(self.root.getSignature())
         self.mapTreeWithNetworkx(graph, self.root)
 
-        pos = nx.shell_layout(graph)
+        pos = nx.spring_layout(graph)
         nx.draw(graph, pos, with_labels=True, node_size=2000, node_color="skyblue", font_size=12, font_weight="bold", edge_color="gray")
         plt.title("Huffman Tree")
         plt.show()
@@ -187,10 +189,103 @@ class HuffmanTree:
         return chr(int(value, 2))
 
 
-if __name__ == '__main__':
-    #tree = HuffmanTree('iasogsdioasonaspvasdcpoiasnvaplsklmcojsiscpkasmca')
+class HuffmanBinaryStringConverter:
+    def restoreSavedTree(folderName:str):
+        huffmanDictionary = {}
+        with open(f'Examples/{folderName}/HuffmanDictionary.json', 'r') as file:
+            huffmanDictionary = json.load(file)
+
+        weightDictionary = {}
+        with open(f'Examples/{folderName}/WeightDictionary.json', 'r') as file:
+            weightDictionary = json.load(file)
+        
+        text = ''
+        with open(f'Examples/{folderName}/text.txt', 'r') as file:
+            text = file.read()
+
+        binaryText = ''
+        with open(f'Examples/{folderName}/text_binary.txt', 'r') as file:
+            binaryText = file.read()
+
+        huffmanText = ''
+        with open(f'Examples/{folderName}/text_binary_huffman.txt', 'r') as file:
+            huffmanText = file.read()
+
+        actualTree = Node(None, 0)
+        for i in huffmanDictionary:
+            currentNode = actualTree
+
+            for j in huffmanDictionary[i]:
+
+                if j == '0':
+                    if currentNode.getLeftBranch() is None:
+                        blankNode = Node(None, 0)
+                        currentNode.assignLeft(blankNode)
+                        currentNode = blankNode
+                    else:
+                        currentNode = currentNode.getLeftBranch()
+
+                else:
+                    if currentNode.getRightBranch() is None:
+                        blankNode = Node(None, 0)
+                        currentNode.assignRight(blankNode)
+                        currentNode = blankNode
+                    else:
+                        currentNode = currentNode.getRightBranch()
+
+            currentNode.value = i
+            currentNode.weight = weightDictionary[i]
+
+        translatedText = ''
+        currentNode = actualTree
+
+        for i in huffmanText:
+            if currentNode.getLeftBranch() is None and currentNode.getRightBranch() is None:
+                translatedText += currentNode.value
+                currentNode = actualTree
+                continue
+
+            if i == '0':
+                currentNode = currentNode.getLeftBranch()
+
+            else:
+                currentNode = currentNode.getRightBranch()
+        
+        print(translatedText)
+
+
+def runTestingForFolder(folderName:str):
+    print(f'\n\n----- {folderName} Test -----')
     text = ''
-    with open('bee_movie.txt', 'r') as file:
+    with open(f'Examples/{folderName}/text.txt', 'r') as file:
         text = file.read()
     tree = HuffmanTree(text)
-    tree.plotTreeBasic()
+
+    with open(f'Examples/{folderName}/HuffmanDictionary.json', 'w') as file:
+        json.dump(tree.huffmanDictionary, file, indent=4)
+
+    with open(f'Examples/{folderName}/WeightDictionary.json', 'w') as file:
+        json.dump(tree.weightDictionary, file, indent=4)
+
+    huffmanResult = tree.convertIntoHuffmanBinary()
+    with open(f'Examples/{folderName}/text_binary_huffman.txt', 'w') as file:
+        file.write(huffmanResult)
+    
+    binaryResult = tree.convertIntoBinary()
+    with open(f'Examples/{folderName}/text_binary.txt', 'w') as file:
+        file.write(binaryResult)
+
+    print(f'Original String: {len(text)} characters')
+    print(f'ASCII Binary String: {len(binaryResult)} bits')
+    print(f'Huffman Binary String: {len(huffmanResult)} bits')
+
+def restore():
+    HuffmanBinaryStringConverter.restoreSavedTree('bee_movie')
+
+if __name__ == '__main__':
+    #runTestingForFolder('bee_movie')
+    #runTestingForFolder('lorem_ipsum')
+    HuffmanBinaryStringConverter.restoreSavedTree('bee_movie')
+    #beeMovieTest()
+    #loremIpsumTest()
+    #restore()
