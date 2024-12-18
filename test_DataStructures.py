3ce13766b29cf48dd4327d781d9b042d9db17bd3
Robson <robsonluan273@hotmail.com>
2024-12-18 20:36:39 -0300

    added basic tests for the tree

--- test_DataStructures.py
+++ test_DataStructures.py
@@ -43,6 +43,30 @@ def sample_nodes(sample_tree_schema) -> Node:
 def test_root_node_signature_correctness(sample_tree_schema, sample_nodes):
     assert Node(sample_tree_schema[0][0], sample_tree_schema[0][1]).getSignature() == sample_nodes.getSignature()
 
+def test_root_left_branch_signature_correctness(sample_tree_schema:list, sample_nodes:Node):
+    leftBranch = sample_tree_schema[1]
+    if type(leftBranch) is list:
+        leftBranch = leftBranch[0]
+    leftBranch = Node(leftBranch[0], leftBranch[1])
+
+    assert leftBranch.getSignature() == sample_nodes.getLeftBranch().getSignature()
+
+def test_root_right_branch_signature_correctness(sample_tree_schema:list, sample_nodes:Node):
+    rightBranch = sample_tree_schema[2]
+    if type(rightBranch) is list:
+        rightBranch = rightBranch[0]
+    rightBranch = Node(rightBranch[0], rightBranch[1])
+
+    assert rightBranch.getSignature() == sample_nodes.getRightBranch().getSignature()
+
+def test_assign_none_value_on_left_branch(sample_nodes:Node):
+    with pytest.raises(TypeError):
+        sample_nodes.assignLeft(None)
+
+def test_assign_none_value_on_right_branch(sample_nodes:Node):
+    with pytest.raises(TypeError):
+        sample_nodes.assignRight(None)
+
 @pytest.fixture
 def sample_ordered_list() -> BalancedList:
     originalList = [1, 3, 2, 5, 12, 5, 12, 7, 13, 17, 5, 4, 4, 5, 4, 9, 6, 8, 4, 3, 41]
