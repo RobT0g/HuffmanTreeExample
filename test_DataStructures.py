894402589f1b473ee70b9bf012c86f5461240d33
Robson <robsonluan273@hotmail.com>
2024-12-18 21:12:00 -0300

    parametrizing tests

--- test_DataStructures.py
+++ test_DataStructures.py
@@ -1,38 +1,48 @@
 import typing
 import pytest
 from DataStructures import *
+import json
 
 #Node tests
 @pytest.fixture
 def sample_tree_schema() -> list:    
-    treeSchema = [
-        ('A', 27),
-        [
-            (None, 3),
-            ('R', 9),
-            ('B', 1),
-        ], [
-            ('G', 6),
-            ('C', 1),
-            [
-                ('l', 13),
-                ('D', 7), 
-                (' ', 15)
-            ]
+    treeSchema = {
+        "": [
+            ['A', 27],
+            {
+                "": [
+                    [None, 3],
+                    ['R', 9],
+                    ['B', 1],
+                ]
+            }, 
+            {
+                "": [
+                    ['G', 6],
+                    ['C', 1],
+                    {
+                        "": [
+                            ['l', 13],
+                            ['D', 7], 
+                            [' ', 15]
+                        ]
+                    }
+                ]
+            }
         ]
-    ]
+    }
 
     return treeSchema
 
 @pytest.fixture
-def sample_nodes(sample_tree_schema) -> Node:
-    def depthFirstNav(currentStep:tuple|list) -> Node:
-        if type(currentStep) is tuple: 
+def sample_tree_created_with_original(sample_tree_schema) -> Node:
+    def depthFirstNav(currentStep:list|dict) -> Node:
+        if type(currentStep) is list: 
             return Node(currentStep[0], currentStep[1])
         
-        actualNode = Node(currentStep[0][0], currentStep[0][1])
-        actualNode.assignLeft(depthFirstNav(currentStep[1]))
-        actualNode.assignRight(depthFirstNav(currentStep[2]))
+        actualNode = Node(currentStep[""][0][0], currentStep[""][0][1])
+        actualNode.assignLeft(depthFirstNav(currentStep[""][1]))
+        actualNode.assignRight(depthFirstNav(currentStep[""][2]))
 
         return actualNode
 
@@ -40,33 +50,43 @@ def sample_nodes(sample_tree_schema) -> Node:
     actualTree = depthFirstNav(sample_tree_schema)
     return actualTree
 
-def test_root_node_signature_correctness(sample_tree_schema, sample_nodes):
-    assert Node(sample_tree_schema[0][0], sample_tree_schema[0][1]).getSignature() == sample_nodes.getSignature()
+@pytest.fixture
+def sample_tree_created_from_json_string(sample_tree_schema) -> Node:
+    return Node.fromJSONString(json.dumps(sample_tree_schema))
 
-def test_root_left_branch_signature_correctness(sample_tree_schema:list, sample_nodes:Node):
-    leftBranch = sample_tree_schema[1]
-    if type(leftBranch) is list:
-        leftBranch = leftBranch[0]
+@pytest.mark.parametrize("sample_tree", ['sample_tree_created_with_original', 'sample_tree_created_from_json_string'])
+def test_root_node_signature_correctness(sample_tree_schema:dict, sample_tree:Node):
+    assert Node(sample_tree_schema[""][0][0], sample_tree_schema[""][0][1]).getSignature() == sample_tree.getSignature()
+
+@pytest.mark.parametrize("sample_tree", ['sample_tree_created_with_original', 'sample_tree_created_from_json_string'])
+def test_root_left_branch_signature_correctness(sample_tree_schema:list, sample_tree:Node):
+    leftBranch = sample_tree_schema[""][1]
+    if type(leftBranch) is dict:
+        leftBranch = leftBranch[""][0]
     leftBranch = Node(leftBranch[0], leftBranch[1])
 
-    assert leftBranch.getSignature() == sample_nodes.getLeftBranch().getSignature()
+    assert leftBranch.getSignature() == sample_tree.getLeftBranch().getSignature()
 
-def test_root_right_branch_signature_correctness(sample_tree_schema:list, sample_nodes:Node):
-    rightBranch = sample_tree_schema[2]
-    if type(rightBranch) is list:
-        rightBranch = rightBranch[0]
+@pytest.mark.parametrize("sample_tree", ['sample_tree_created_with_original', 'sample_tree_created_from_json_string'])
+def test_root_right_branch_signature_correctness(sample_tree_schema:list, sample_tree:Node):
+    rightBranch = sample_tree_schema[""][2]
+    if type(rightBranch) is dict:
+        rightBranch = rightBranch[""][0]
     rightBranch = Node(rightBranch[0], rightBranch[1])
 
-    assert rightBranch.getSignature() == sample_nodes.getRightBranch().getSignature()
+    assert rightBranch.getSignature() == sample_tree.getRightBranch().getSignature()
 
-def test_assign_none_value_on_left_branch(sample_nodes:Node):
+@pytest.mark.parametrize("sample_tree", ['sample_tree_created_with_original', 'sample_tree_created_from_json_string'])
+def test_assign_none_value_on_left_branch(sample_tree:Node):
     with pytest.raises(TypeError):
-        sample_nodes.assignLeft(None)
+        sample_tree.assignLeft(None)
 
-def test_assign_none_value_on_right_branch(sample_nodes:Node):
+@pytest.mark.parametrize("sample_tree", ['sample_tree_created_with_original', 'sample_tree_created_from_json_string'])
+def test_assign_none_value_on_right_branch(sample_tree:Node):
     with pytest.raises(TypeError):
-        sample_nodes.assignRight(None)
+        sample_tree.assignRight(None)
 
+#BalancedList tests
 @pytest.fixture
 def sample_ordered_list() -> BalancedList:
     originalList = [1, 3, 2, 5, 12, 5, 12, 7, 13, 17, 5, 4, 4, 5, 4, 9, 6, 8, 4, 3, 41]
