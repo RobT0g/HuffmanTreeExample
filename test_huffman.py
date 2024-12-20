188fd090b2a27d648bb718dfcbacf7c8913a6227
Robson <robsonluan273@hotmail.com>
2024-12-19 22:49:13 -0300

    tests are broken, adding contexted parametrization

--- test_huffman.py
+++ test_huffman.py
@@ -10,7 +10,7 @@ pytestmark = pytest.mark.parametrize(
     ]
 )
 
-def getSampleText(folderPath: str) -> str:
+def __getSampleText(folderPath: str) -> str:
     return open(f'Examples/{folderPath}/text.txt', 'r').read()
 
 def test_can_instantiate_huffman_tree(sample_folder:str, sample_huffman_tree:HuffmanTree):
@@ -22,7 +22,7 @@ def test_is_reading_text_file(sample_folder:str, sample_huffman_tree:HuffmanTree
     assert sample_huffman_tree.originalText == getSampleText(sample_folder)
 
 def test_can_get_character_count(sample_folder:str, sample_huffman_tree:HuffmanTree):
-    sample_text = getSampleText(sample_folder)
+    sample_text = __getSampleText(sample_folder)
     count = HuffmanTree.getCharacterCount(sample_text)
     actualCount = {}
     for char in sample_text:
@@ -33,4 +33,31 @@ def test_can_get_character_count(sample_folder:str, sample_huffman_tree:HuffmanT
     for i in actualCount:
         assert count[i] == actualCount[i]
     
+@pytest.mark.parametrize(
+    ('characters', 'binary'),
+    [
+        ('a', '01100001'),
+        ('b', '01100010'),
+        (' ', '00100000'),
+        ('!', '00100001'),
+        ('oikasnd', '01101111011010010110101101100001011100110110111001100100'),
+        ('haters gonna hate', '01101000011000010111010001100101011100110111010000100000011001110110000101101110011011110111010001101000011000010111010001100101')
+    ]
+)
+def test_can_convert_char_to_binary(characters:str, binary:str):
+    assert HuffmanTree.nonHuffman_convertFromCharToBinary(characters) == binary
+
+@pytest.mark.parametrize(
+    ('characters'),
+    [
+        (''),
+        (None),
+        ({}),
+        ([])
+    ]
+)
+def test_invalid_character_raises_error(characters:any):
+    with pytest.raises(ValueError):
+        HuffmanTree.nonHuffman_convertFromCharToBinary(characters)
+
 
